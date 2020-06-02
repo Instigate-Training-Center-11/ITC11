e97f64cb3dc13296572d8cf7f3b42cfbff4189cb
ArmenNersisyan1995 <armennersesyan95@gmail.com>
2020-06-03 02:37:30 +0400

    Added new new files "factorial.cpp" and reverseString

---
+++ c++/02_06_2020/factorial.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+/* function declaration factorialR, recursive calculates and returns the factorialR of the passed number */
+unsigned long long int factorialR(int number);
+
+/* function declaration factorialP, positive calculates and returns the factorialP of the passed number */
+unsigned long long int factorialP(int number);
+
+int main() {
+    unsigned int number = 0;
+
+    do {
+        std::cout << "input a positive number: ";
+        std::cin >> number;
+    } while (number < 3);
+
+    std::cout << "\ncall factorialP function\n\n";
+    std::cout << "input number = "<< number << "\nResult\nfactorial = " << factorialP(number) << "\n\n";
+    std::cout << "call factorialR function\n\n";
+    std::cout << "input number = "<< number << "\nResult\nfactorial = " << factorialR(number) << "\n\n";
+
+    return 0;
+}
+
+/* implementation of the factorialR function, recursive calculates and returns the factorialR of the passed number */
+unsigned long long int factorialR(int number) {
+    if (number == 2) {
+        return 2;
+    }
+    return number * factorialR(--number);
+}
+
+/* implementation of the factorialP function, positive calculates and returns the factorialP of the passed number */
+unsigned long long int factorialP(int number) {
+    unsigned long long int factorial = 1;
+    for (int i = 2; i <= number; i++) {
+        factorial *= i;
+    }
+    return factorial;
+}
---
+++ c++/02_06_2020/reverseString.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+/* function  declaration reverseString, gets the argument string and will do reversed */
+void reverseString(char* str);
+
+int main() {
+    const int SIZE = 200;
+    char arr[SIZE];
+
+    std::cout << "Please, enter your text: ";
+
+    /* input text passed in array (arr) */
+    std::cin.getline(arr, SIZE);
+    char *text = arr;
+    std::cout << "\n\t\tInput string\n" <<text << std::endl;
+    /* call function reverseString */
+    reverseString(text);
+    std::cout << "\t\tReversed string\n" << text << std::endl;
+
+    return 0;
+}
+
+/* implementation reverseString, gets the argument string and will do reversed */
+void reverseString(char* str) {
+    int length = 0;
+    /* gets the number of characters in a string */
+    while (str[length] != '\0') {
+        ++length;
+    }
+    for (int i = 0, j = length - 1; i < length, j >= length; i++, --j) {
+        str[i] = str[i] ^ str[j];
+        str[j] = str[i] ^ str[j];
+        str[i] = str[i] ^ str[j];
+
+        /* both do the same thing, swapping characters
+        str[i] = str[i] ^ str[j];
+        str[j] = str[i] ^ str[j];
+        str[i] = str[i] ^ str[j]; */
+    }
+}
