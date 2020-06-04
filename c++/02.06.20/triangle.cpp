c4f417792e0e8afa64724df571f0ffe22c7054e1
Jemma <jemma.koninyan89@gmail.com>
2020-06-04 13:55:45 +0400

    Correct the mistakes

--- c++/02.06.20/triangle.cpp
+++ c++/02.06.20/triangle.cpp
@@ -2,42 +2,41 @@
 
 /* checks input number is integer or not */
 int type(int num) {
-    while(std::cin.fail()) {
+    while (std::cin.fail() || num < 2) {
         std::cin.clear();
-        std::cin.ignore();
+        std::cin.ignore(100,'\n');
         std::cout << "Enter a positive number: ";
         std::cin >> num;
     }
-            return num;
+    return num;
 }
 
 /*Drawing triangle*/
 void triangle(int row) {
-    for(int i = 1; i <= row; i++) {
-        for(int j = 1; j < i; j++) {
+    for (int i = 1; i <= row; i++) {
+        for (int j = 1; j < i; j++) {
             std::cout << " ";
         }
-        for(int j = i; j <= 2 * row - 2; j++) {
+        for (int j = i; j <= 2 * row - 1; j++) {
             if ((i == 1 && j % 2 == 1) || j == i || i + j == 2 * row) {
-                std::cout << "* ";
+                std::cout << "*";
             } else {
                 std::cout << " ";
             }
         }
-       std::cout << std::endl;
-     }
+        std::cout << std::endl;
+    }
 }
 
 int main() {
     int row = 0;
     /*Entered a number of rows for triangle*/
-    while (row < 1) {
-        std::cout << "Please enter a number of rows you want to see: " << std::endl;
-        std::cin >> row;
-        std::cout << "Rows: " << row << std::endl;
-        int number = type(row);
-    }
-    triangle(row);
+    std::cout << "Please enter a number of rows you want to see: " << std::endl;
+    std::cin >> row;
+
+    int number = type(row);
+    std::cout << "Rows: " << number << std::endl;
+    triangle(number);
     return 0;
 }
 
