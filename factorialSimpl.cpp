e7d3e5c1871862f195108243ad3d7c2d2e8c6952
ArmenuhiKocharyan <armamik@gmail.com>
2020-06-04 21:18:05 +0400

    Added file factorialSimpl.cpp into remote repository

---
+++ factorialSimpl.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+// function to find factorial of given number
+unsigned int factorialRec(unsigned int n) {
+    if (n == 0) {
+        return 1;
+    }
+    return n * factorialRec(n - 1);
+}
+
+unsigned int factorialWRec(unsigned int n) {
+    unsigned result = 1;
+    for (double i = 2; i <= n; ++i) {
+        result *= i;
+    }
+    return result;
+}
+
+int main() {
+    unsigned int num;
+    std::cout << "Enter the positive number: ";
+    std::cin >> num;
+    std::cout << "Factorial with recursion of "
+              << num << " is " << factorialRec(num) << std::endl;
+    std::cout << "Factorial without recursion of "
+              << num << " is " << factorialWRec(num) << std::endl;
+    return 0;
+}
