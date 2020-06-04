462d2dc4b0fa979c73a6d841f57a6d8f180c91dc
Grik Tadevosyan <arthuriktadevosyan@gmail.com>
2020-06-05 02:00:28 +0400

    First

---
+++ c++/Homework04.06.2020/LinkedList.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+class Node{
+    public:
+        int data;
+        Node* next;
+};
+
+class LinkedList{
+    public:
+    LinkedList(){
+        head=NULL;
+    }
+    ~LinkedList();
+    void insert(int val);
+    void display();
+    void deleteElement(int val);
+    void empty();
+    int getElementByIndex(int index);
+    private:
+    Node* head;
+};
+void LinkedList::insert(int val) {
+    Node* newnode=new Node();
+    newnode->data=val;
+    newnode->next=NULL;
+    if(head==NULL){
+        head=newnode;
+    }
+    else
+    {
+        Node* temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newnode;
+    }
+    
+}
+int LinkedList::getElementByIndex(int index){
+  Node* current = head;
+  int count=0;
+  while(current!=NULL){
+      if(count==index)
+      return current->data;
+      count++;
+      current=current->next;
+  }
+  return -1;
+}
+void LinkedList::deleteElement(int index){
+  if(head==NULL)
+  return;
+  Node* temp=head;
+  if(index==0){
+      head=temp->next;
+      free(temp);
+      return;
+  }
+  for(int i=0;temp!=NULL && i<index-1;i++)
+      temp=temp->next;
+      if(temp==NULL || temp->next==NULL)
+      return;
+      Node *next=temp->next->next;
+      free(temp->next);
+      temp->next=next;
+
+  
+}
+void LinkedList::empty(){
+    head=NULL;
+}
+void LinkedList::display() {
+    if(head==NULL){
+        std::cout<<"List is empty!"<<'\n';
+
+    }
+   else {
+        Node* temp=head;
+        while (temp!=NULL)
+        {
+            std::cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+        std::cout<<'\n';
+        
+   }
+}
+int main(){
+    LinkedList* list=new LinkedList();
+    list->insert(145);
+    list->insert(147);
+    list->insert(108);
+    std::cout<<"Second element is "<<list->getElementByIndex(1)<<'\n';
+    list->deleteElement(1);
+    std::cout<<"Linked List:"<<'\n';
+    list->display();
+    return 0;
+}
