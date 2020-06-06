73c5a8849224f4758ed79872ee9bcbb75f2086fd
Jemma <jemma.koninyan89@gmail.com>
2020-06-06 14:42:41 +0400

    Delete copy constructor

--- c++/04.06.20/list.hpp
+++ c++/04.06.20/list.hpp
@@ -1,18 +1,25 @@
 #ifndef LIST_HPP
 #define LIST_HPP
 
-class List
-{
+class List {
     public:
-        List(void);
-        List(List& list);
-        ~List(void);
+        /*Default constructor*/
+        List();
+        /*Destructor*/
+        ~List();
+        /*Insert new node to list by index*/
         void add(int val, int index);
+        /*Delete node from list by index*/
         void remove(int index);
+        /*Get element from list by index*/
         int getElement(int index);
+        /*Check the list empty or not*/
         bool isEmpty();
+        /*Get the size of list*/
         int getSize();
+        /*Display the list*/
         void printf();
+
     private:
         struct Node {
             int value;
