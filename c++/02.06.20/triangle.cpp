79c7cb47c5662b6ade2bddf62c0e003ed5de4ead
Jemma <jemma.koninyan89@gmail.com>
2020-06-06 13:34:59 +0400

    Correct marked mistakes

--- c++/02.06.20/triangle.cpp
+++ c++/02.06.20/triangle.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
-/* checks input number is integer or not */
-int type(int num) {
+/* Checks input number is integer or symbol */
+int type (int num) {
+    std::cout << "Please enter a number of rows you want to see: ";
+    std::cin >> num;
     while (std::cin.fail() || num < 2) {
+        std::cout << "ERROR: Faulty input! Try again!" << std::endl;
         std::cin.clear();
         std::cin.ignore(100,'\n');
         std::cout << "Enter a positive number: ";
@@ -12,11 +15,12 @@ int type(int num) {
 }
 
 /*Drawing triangle*/
-void triangle(int row) {
+void triangle (int row) {
     for (int i = 1; i <= row; i++) {
         for (int j = 1; j < i; j++) {
             std::cout << " ";
         }
+        /*The sides of triangle*/
         for (int j = i; j <= 2 * row - 1; j++) {
             if ((i == 1 && j % 2 == 1) || j == i || i + j == 2 * row) {
                 std::cout << "*";
@@ -28,12 +32,9 @@ void triangle(int row) {
     }
 }
 
-int main() {
+int main () {
     int row = 0;
     /*Entered a number of rows for triangle*/
-    std::cout << "Please enter a number of rows you want to see: " << std::endl;
-    std::cin >> row;
-
     int number = type(row);
     std::cout << "Rows: " << number << std::endl;
     triangle(number);
