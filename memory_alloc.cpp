61c6018db3ff228450372e7b0ce9d495573a8709
GrikTad <52489055+GrikTad@users.noreply.github.com>
2020-06-07 12:27:39 +0400

    Add files via upload
    This is homework at 03.06.2020

---
+++ memory_alloc.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+ void swapp(int a, int b);
+ void swappRef(int &a, int &b);
+int main(){
+    int a=145;
+    int* b=NULL;
+    b=&a;
+    std::cout<<a<<'\n';
+    std::cout<<b<<'\n'; //will print address of b
+    std::cout<<*b<<'\n'; //will print 145
+    int first=14;
+    int second=15;
+    std::cout<<"first after swapping\n"<<first<<'\n'<<"secoond after swapping\n"<<second<<'\n';
+    swapp(first,second);
+    std::cout<<"first and second after swapping with no &\n";
+    std::cout<<first<<' '<<second<<'\n';
+    swappRef(first,second);
+    std::cout<<"first and second values after swapping with &\n";
+    std::cout<<first<<" "<<second<<'\n';
+    return 0;
+}
+void swapp(int a, int b){
+  int temp=a;
+  a=b;
+  b=temp;
+  std::cout<<a<<' '<<b<<'\n'; //this prints a and b values after swapping
+}
+void swappRef(int &a, int &b){
+   int temp=a;
+   a=b;
+   b=temp;
+}
