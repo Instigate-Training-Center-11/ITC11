ca9f85e115c7488120eebcf5cc98e1f4708d9f1b
ArmenNersisyan1995 <armennersesyan95@gmail.com>
2020-06-10 13:16:16 +0400

    Add new folder "classException"

---
+++ c++/09_06_2020/classException/main.cpp
@@ -0,0 +1,38 @@
+#ifndef _IOSTREAM_
+#define _IOSTREAM_
+
+#include "queue.hpp"
+#include "list.hpp"
+
+int main() {
+
+    List<int> list;
+    Queue<int> obj(4);
+    for (int i = 0; i < 10; ++i){
+        list.insertAtIndex(i,i);
+    }
+
+    //std::cout << object.Size() << std::endl;
+    /* index,value */
+        list[8000];
+        //std::cerr << "An array exception occurred (" << exception.getError() << ")\n";
+    //list.printList();
+    list.removeAtIndex(0);
+    list.printList();
+    //list.clear();
+    //obj.push(8);
+    //obj.show();
+    //obj.size();
+    //obj.pop();
+    //obj.show();
+    //list.printList();
+    //list.insertAtIndex(0,1000);
+    //list.setAtIndex(5,20);
+    //list.removeAtIndex(8);
+    //list.getSize();
+    //list[5];
+
+    return 0;
+}
+
+#endif
---
+++ c++/09_06_2020/classException/queue.hpp
@@ -0,0 +1,22 @@
+#ifndef _QUEUE_H
+#define _QUEUE_H
+
+#include "list.hpp"
+#include "exception.hpp"
+
+template <typename Type>
+
+class Queue : protected List<Type> {
+public:
+    Queue();
+    Queue(int);
+    void show(void);
+    void size(void);
+    void pop(void);
+    void push(Type);
+    ~Queue();
+};
+
+#include "queue.cpp"
+
+#endif
