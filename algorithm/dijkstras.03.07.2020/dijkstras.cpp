03f2b9cc734409d9f8b14eb504d94da0ad678608
ArmenNersisyan1995 <armennersesyan95@gmail.com>
2020-07-06 14:17:24 +0400

    Update

--- algorithm/dijkstras.03.07.2020/dijkstras.cpp
+++ algorithm/dijkstras.03.07.2020/dijkstras.cpp
@@ -48,28 +48,42 @@ int main(){
 
 void DijkstrasShortestPath (int cityOne, int cityTwo, VVPAIR& graph) {
     int nodeCount = graph.size();
+    std::vector<int> pointArray;
     const long long INF = 999999999999;
     std::vector<unsigned long long> dist(nodeCount, INF);
-    std::set<PII> set_length_node;
+    std::vector<PII> distWAy(nodeCount);
+    PII data = *distWAy.begin();
+    std::set<PII> setLengthNode;
     dist[cityOne] = 0;
-    set_length_node.insert(PII(0,cityOne));
-    while (!set_length_node.empty()) {
+    setLengthNode.insert(PII(0,cityOne));
+    while (!setLengthNode.empty()) {
 
-        PII top = *set_length_node.begin();
-        set_length_node.erase(set_length_node.begin());
+        PII top = *setLengthNode.begin();
+        setLengthNode.erase(setLengthNode.begin());
         int cityOne = top.second;
         for(auto& it : graph[cityOne]) {
-            int adj_node = it.first;
+            int adjNode = it.first;
             int length_to_adjnode = it.second;
-            if (dist[adj_node] > length_to_adjnode + dist[cityOne]) {
-                if (dist[adj_node] != INF) {
-                   set_length_node.erase(set_length_node.find(PII(dist[adj_node],adj_node)));
+            if (dist[adjNode] > length_to_adjnode + dist[cityOne]) {
+                if (dist[adjNode] != INF) {
+                   setLengthNode.erase(setLengthNode.find(PII(dist[adjNode],adjNode)));
                 }
-                dist[adj_node] = length_to_adjnode + dist[cityOne];
-                set_length_node.insert(PII(dist[adj_node], adj_node));
+                dist[adjNode] = length_to_adjnode + dist[cityOne];
+                data.first = dist[adjNode];
+                data.second = cityOne;
+                distWAy[adjNode] = data;
+                setLengthNode.insert(PII(dist[adjNode], adjNode));
             }
         }
     }
+    PII temp = distWAy[cityTwo];
+    pointArray.push_back(cityTwo);
+    while (temp.second != 0 && temp.first != 0) {
+        pointArray.push_back(temp.second);
+        temp = distWAy[temp.second];
+    }
+
+    std::cout <<"\n" << pointArray.size();
     for (int i = 0; i < nodeCount; i++) {
         switch(i) {
             case 0:
@@ -92,6 +106,10 @@ void DijkstrasShortestPath (int cityOne, int cityTwo, VVPAIR& graph) {
                 break;
         }
     }
-
-        std::cout << "\nSource Node(" << cityOne << ") -> Destination Node(" << cityTwo << ") : " << dist[cityTwo] << " km" <<std::endl;
+    std::cout << "\ncityOne(" << cityOne << ") -> cityTwo(" << cityTwo << ") : " << dist[cityTwo] << " km" <<std::endl;
+    std:: cout << "\t\tthe shortest road\n\t\t";
+    std::cout << cityOne;
+    for (int k = pointArray.size() - 1; k >= 0; --k) {
+        std::cout <<"->" << pointArray[k];
+    }
 }
