aa6d4dea2f1c8f16e13b49c08e3c5ec8daff3070
ArmenuhiKocharyan <armamik@gmail.com>
2020-07-13 01:44:46 +0400

    Added new version of QuickSort

--- 30.06.2020./QuickSort.cpp
+++ 30.06.2020./QuickSort.cpp
@@ -1,40 +1,46 @@
 #include <iostream>
 using namespace std;
 
-int size = 0;
-
 /* create function for swap elements of array */
-void swap(int &a, int &b) {
+void swap(int &a, int &b)
+{
     int t = a;
     a = b;
     b = t;
 }
 
 /* create function for display elements of array */
-void display(int arr[]) {
+void display(int arr[], int size)
+{
     cout << "Displaying elements: " << endl;
     /* display array elements */
-    for (int i = 0; i < size; ++i) {
+    for (int i = 0; i < size; ++i)
+    {
         cout << "Element " << i + 1 << ": " << arr[i] << endl;
     }
 }
 /* we have subunits for elements gretter of pivot and smaller of pivot */
 /* for my realization I selected for pivot value the first element of array */
-int subunit(int arr[], int left, int right) {
+int subunit(int arr[], int left, int right)
+{
     int pivot = arr[left];
     int start = left;
     int end = right;
 
     /* we nned to increment from start element, while elements are small then pivot */
     /* and we need to decrement from end, while elements are greater, then pivot */
-    while (start < end) {
-        while (arr[start] <= pivot) {
+    while (start < end)
+    {
+        while (arr[start] <= pivot)
+        {
             start++;
         }
-        while (arr[end] > pivot) {
+        while (arr[end] > pivot)
+        {
             end--;
         }
-        if (start < end) {
+        if (start < end)
+        {
             swap(arr[start], arr[end]);
         }
     }
@@ -42,19 +48,36 @@ int subunit(int arr[], int left, int right) {
     return end;
 }
 
-void quickSort(int arr[], int left, int right) {
-    if (left < right) {
+void quickSort(int arr[], int left, int right)
+{
+    if (left < right)
+    {
         int local = subunit(arr, left, right);
         quickSort(arr, left, local - 1);
         quickSort(arr, local + 1, right);
     }
 }
 
-int main() {
-    size = 6;
-    int arr[size] = {7, 4, 10, 8, 3, 1};
-    quickSort(arr, arr[0], arr[size]);
-    display(arr);
-    cout << endl;
+int main()
+{
+    int size;
+    cout << "Enter the size of array: " << endl;
+    cin >> size;
+    int *arr = new int[size];
+    if (size <= 0)
+    {
+       cout << "Size will not be equal or les then zero." << endl;
+    }
+    else
+    {
+        for (int i = 0; i < size; i++)
+        {
+            cout << "Enter the (next) number " << endl;
+            cin >> arr[i];
+        }
+        quickSort(arr, 0, size - 1);
+        display(arr, size);
+        cout << endl;
+    }
     return 0;
 }
