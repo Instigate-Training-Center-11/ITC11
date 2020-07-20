2d01e1e08e44fe8fc0be52c09845223783983c39
mkolibri <maneantonyan@gmail.com>
2020-07-21 02:05:51 +0400

    Added get Valid number count task final version

--- python/day1/getNumber.py
+++ python/day1/getNumber.py
@@ -7,10 +7,31 @@ def getNumber():
             number = int(input())
         except ValueError:
             print("It is not a number!")
+            print("Try again: ", end = "")
             number = 0
         else:
             if number < 1:
                 print("Number must be great then 0!")
+                print("Try again: ", end = "")
+                number = 0
+            else:
+                return number
+
+# Enter only integer number(not other charecter)
+# Number must be in range [100, 999]
+def getThreeDigitNumber():
+    number = 0
+    while number < 100 or number > 999:
+        try:
+            number = int(input())
+        except ValueError:
+            print("It is not a number!")
+            print("Try again: ", end = "")
+            number = 0
+        else:
+            if number < 100 or number > 999:
+                print("Number must have 3 digits!")
+                print("Try again: ", end = "")
                 number = 0
             else:
                 return number
