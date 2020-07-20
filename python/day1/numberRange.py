ffba250a7fa8a305362782b77a443243c071966d
mkolibri <maneantonyan@gmail.com>
2020-07-21 01:19:06 +0400

    Added number Range task final version

---
+++ python/day1/numberRange.py
@@ -0,0 +1,22 @@
+# Enter only integer number(not other charecter)
+def getNumber():
+    number = 0
+    while number < 1:
+        print('Enter number: ', end = "")
+        try:
+            number = int(input())
+        except ValueError:
+            print("It is not a number!")
+            number = 0
+        else:
+            return number
+
+# Main function to check range of number
+def main():
+    number = getNumber()
+    condition = number == 1100 or number == 900
+    condition = condition or number == 2100 or number == 1900
+    print(True if condition else False)
+
+if __name__ == "__main__":
+    main()
