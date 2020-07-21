6c3862d3363cb22e355e78c092bbd62d6aac037a
ArmenuhiKocharyan <armamik@gmail.com>
2020-07-21 23:18:07 +0400

    camelcase to underscore

--- 20.07.2020./PythonFunctions.py
+++ 20.07.2020./PythonFunctions.py
@@ -18,23 +18,23 @@ print("=======================Pascal triangle=======================")
 
 n = int(input("Enter the number of rows : "))
 
-def pascalLine(row, col):
+def pascal_line(row, col):
     if col == 1:
         return 1
     if col == row:
         return 1
-    left = pascalLine(row - 1, col - 1)
-    right = pascalLine(row - 1, col)
+    left = pascal_line(row - 1, col - 1)
+    right = pascal_line(row - 1, col)
     return left + right
 
 for r in range(1, n + 1):
     for c in range(1, r + 1):
-        print(pascalLine(r, c), end = " ")
+        print(pascal_line(r, c), end = " ")
     print('')
 
 print("=======================Count of local variables=======================")
 
-def printingName():
+def printing_name():
     firstname = "Vardan"
     lastname = "Babayan"
     print(firstname + " " + lastname)
@@ -45,7 +45,7 @@ def addition():
     third = 5
     print(first + second + third)
 
-n = printingName.__code__.co_nlocals
+n = printing_name.__code__.co_nlocals
 m = addition.__code__.co_nlocals
 print("The count of local variables of printName() is: " + str(n))
 print("The count of local variables of addition() is: " + str(m))
@@ -54,10 +54,10 @@ print("=======================Is in range? =======================")
 
 n = int(input("Enter the number : "))
 
-def isInRange(n):
+def is_in_range(n):
     return ((abs(1000 - n) <= 100) or (abs(2000 - n) <= 100))
 
-print(isInRange(n))
+print(is_in_range(n))
 
 print("=======================Is perfect number? =======================")
 
@@ -65,25 +65,25 @@ n = int(input("Enter the number : "))
 while n < 0 or n % 2 > 0:
     n = int(input("Can not be a negative or an odd number : "))
 
-def isPerfect(n):
+def is_perfect(n):
     total = 0
     for x in range(1, n):
         if n % x == 0:
             total += x
     return total == n
 
-if isPerfect(n) == True:
-    print(str(isPerfect(n)) + " : " + str(n) + " is a perfect number.")
+if is_perfect(n) == True:
+    print(str(is_perfect(n)) + " : " + str(n) + " is a perfect number.")
 else:
-    print(str(isPerfect(n)) + " : " + str(n) + " is not a perfect number.")
+    print(str(is_perfect(n)) + " : " + str(n) + " is not a perfect number.")
 
 print("=======================Three-digit number=======================")
 
 n = int(input("Enter the three-digit number for range : "))
-listOfNums = []
+list_of_nums = []
 for number in range(100, n):
     if number % 3 == 0:
-        listOfNums.append(number)
-print(len(listOfNums))
+        list_of_nums.append(number)
+print(len(list_of_nums))
 
 print("=======================End=======================")
