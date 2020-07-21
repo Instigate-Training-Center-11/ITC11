3910ec25e6d925ae2b7d1cbe5e676c5845e12c6d
mkolibri <maneantonyan@gmail.com>
2020-07-22 02:00:57 +0400

    Added minor fixes

--- python/day2/sortStrings/main.py
+++ python/day2/sortStrings/main.py
@@ -3,7 +3,7 @@ import functions as module
 def main():
     ''' Line count must be great then 0 '''
     print("enter number of lines: ", end = "")
-    size = module.getNumber()
+    size = module.get_number()
 
     ''' Create dictionary for string-values '''
     strings = {}
@@ -21,17 +21,17 @@ def main():
         else:
             strings[key] = value
 
-    ''' Print numeric-value dictionary sorted '''
-    while len(numbers) > 0:
-        key = module.getMinKey(numbers)
-        print(key, ":", numbers[key])
-        del numbers[key]
-
     ''' Print string-value dictionary sorted '''
     while len(strings) > 0:
-        key = module.getMinKey(strings)
+        key = module.get_min_key(strings)
         print(key, ":", strings[key])
         del strings[key]
+
+    ''' Print numeric-value dictionary sorted '''
+    while len(numbers) > 0:
+        key = module.get_min_key(numbers)
+        print(key, ":", numbers[key])
+        del numbers[key]
 
 if __name__ == "__main__":
     main()
