eaecbe944c1a219ad17aae2452ea5a87a3025186
Artashes <arto1993@github.com>
2020-07-21 14:20:01 +0400

    Update

--- Python/20.07.20/Pascal/main.py
+++ Python/20.07.20/Pascal/main.py
@@ -1,11 +1,3 @@
 import pascal_func
 
-my_array = []
-
-rows = int(input("Insert index: "));
-
-count = 0;
-while count <= rows:
-    pascal_func.pascal_triangle(my_array, count)
-    pascal_func.print_array(my_array)
-    count += 1
+pascal_func.pascal_triangle(int(input("Insert index: ")))
--- Python/20.07.20/Pascal/pascal_func.py
+++ Python/20.07.20/Pascal/pascal_func.py
@@ -1,5 +1,12 @@
+def pascal_triangle(rows):
+    count = 0;
+    while count <= rows:
+        pascal_triangle_rows(count)
+        count += 1
+
 # Calculate current row values
-def pascal_triangle (my_array, row):
+def pascal_triangle_rows(row):
+    my_array = []
     for i in range(row + 1):
         my_array.append(0)
 
@@ -13,8 +20,10 @@ def pascal_triangle (my_array, row):
             i = i - 1
         j = j + 1
 
+    print_array(my_array)
+
 # Print array and clear it
-def print_array (my_array):
+def print_array(my_array):
     for i in my_array:
         print(i, end = ' ')
     my_array.clear()
