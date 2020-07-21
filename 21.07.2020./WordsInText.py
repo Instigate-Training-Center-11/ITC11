2c0ddfaf28c8d3f107adc31de0b51220d44487c4
ArmenuhiKocharyan <armamik@gmail.com>
2020-07-22 01:52:07 +0400

    Added simple solution about recurring words

---
+++ 21.07.2020./WordsInText.py
@@ -0,0 +1,23 @@
+n = int(input("Enter the positive number : "))
+text = str(input("Enter some text : "))
+words = text.split()
+count_of_words = {}
+
+''' Spliting the text '''
+for word in text.split(" "):
+    if len(word) > 0 and word.istitle():
+        ''' Seting the word into count_of_words, if it not in count_of_words and set value to 1 '''
+        if word not in count_of_words:
+            count_of_words[word] = 1
+        else:
+            ''' Incrementing count, if the word already exsist in count_of_words '''
+            count_of_words[word] += 1
+            
+    ''' Sorting the dictionary by the values, reversed, taking all items, if items are less then n'''
+if len(count_of_words) < n:
+    for i, word in enumerate(sorted(count_of_words, key=count_of_words.get, reverse=True)):
+        print("%s: %s - %s" % (i+1, word, count_of_words[word]))
+else:
+    ''' Sorting the dictionary by the values, reversed, taking the n top items,'''
+    for i, word in enumerate(sorted(count_of_words, key=count_of_words.get, reverse=True)[:n]):
+        print("%s: %s - %s" % (i+1, word, count_of_words[word]))
