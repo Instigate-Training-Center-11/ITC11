851aba2da3eb5d5de6ffd70b0b7882f5ddb04039
Jemma <jemma.koninyan89@gmail.com>
2020-07-22 15:50:29 +0400

    rangeNumber.py

---
+++ python/20.07.20/rangeNumber.py
@@ -0,0 +1,19 @@
+def inputUser():
+    num = 0
+    while num < 1:
+        print('Enter the number: ', end = '')
+        try:
+            num = int(input())
+        except ValueError:
+            print('\nYour input is not a number!\n')
+            num = 0
+        else:
+            return num
+def range(num):
+    return ((abs(1000 - num) <= 100) or (abs(2000 - num) <= 100))
+def main():
+    num = inputUser()
+    print(range(num))
+
+if __name__ == "__main__":
+    main()
