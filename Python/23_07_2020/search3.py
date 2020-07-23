8d0ba2895740496e3f14892ef0314bd51c2c0ce1
Meruzhan Unanyan <unanyanmeruzan@gmail.com>
2020-07-24 02:12:04 +0400

    search into files

--- Python/23_07_2020/search3.py
+++ Python/23_07_2020/search3.py
@@ -1,4 +1,19 @@
 import os
+import sys
+
+
+def boolean(s1, s2):
+    if '^' in s1:
+        s = str.replace(s1, '^', '')
+        return s2.startswith(s)
+    elif '*' in s1:
+        s = str.split(s1, '*')
+        return s2.startswith(s[0]) and s2.endswith(s[1])
+    elif '?' in s1:
+        s = str.split(s1, '?')
+        return s2.startswith(s[0]) and s2.endswith(s[1]) and (s2.count("") - s1.count("")) is 0
+    else:
+        return s1 in s2
 
 
 def f(pat, b, w, inc, exc):
@@ -6,13 +21,15 @@ def f(pat, b, w, inc, exc):
     for entry in os.listdir(pat):
         if os.path.isdir(os.path.join(pat, entry)):
             f(os.path.join(pat, entry), b, w, inc, exc)
-            if bb and w in entry and inc in (os.path.join(pat, entry)) and exc not in (os.path.join(pat, entry)):
+            #print(entry)
+            if bb and boolean(w, entry) and inc in (os.path.join(pat, entry)) and exc not in (os.path.join(pat, entry)):
                 print(os.path.join(pat, entry))
         else:
-            if not bb and w in entry and inc in (os.path.join(pat, entry)) and exc not in (os.path.join(pat, entry)):
+            if not bb and boolean(w, entry) and inc in (os.path.join(pat, entry)) and exc not in (os.path.join(pat, entry)):
                 print(os.path.join(pat, entry))
+
 
 
 f(input("--path: "), input("-d or -f: "), input("word or letters in word: "), input("--include : "), input("--exclude: "))
 
-
+#f(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
