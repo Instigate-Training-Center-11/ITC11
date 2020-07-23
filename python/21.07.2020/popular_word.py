82f5346bef814528eac3235fa094e06267ec14b3
Armine <arm.gevorgyan94@gmail.com>
2020-07-24 00:50:28 +0400

    -Modified popular word: Finaly Version

--- python/21.07.2020/popular_word.py
+++ python/21.07.2020/popular_word.py
@@ -1,19 +1,20 @@
+import re
+
 """ the function get lines number
     return line number
 """
-def get_line_count():
-    n = 0
+def get_line_count(string):
     try:
         """try cast input value in int"""
-        n = int(input("Enter Number of lines: "))
+        n = int(input(string))
 
         if(n < 1):
             print("Please enter number grater than 0")
-            get_line_count(n)
+            return get_line_count(string)
     except:
         """when input value is string generate exception and now call get_line_count function"""
         print("Please enter number grater than 0")
-        get_line_count(n)
+        return get_line_count(string)
 
     return n
 
@@ -29,62 +30,88 @@ def input_lines(n, strings):
         strings.append(tmp)
 
 
-def add_popular_word(strings, my_dict):
-    for i in strings:
-        for j in range(0, len(i)):
-            """check word start uppercase or all simbols uppercase"""
-            if(i[j][0].isupper()) and (i[j][1:].islower()) or i[j].isupper():
-                cur = i[j]
-                if(cur[-1] == '.'):
-                    cur = cur[0:(len(cur) - 1)]
-                """chack word find in dictionary and worf finding count incement"""
-                if(cur.upper() in my_dict):
-                    tmp = cur[0] + cur[1:].lower()
-                    indexes = [k for k, x in enumerate(i) if x == cur or x == cur.upper() or x ==  tmp or x == i[j]]
-                    if(indexes[0] != j):
-                        continue
-                    else:
-                        my_dict[cur.upper()] = my_dict[cur.upper()] + 1
-                    
-                #"""when word not found in dictionary """ 
-                #"""check word nots start in line then add in dictionary"""
-                else:
-                    if(j != 0):
-                        tmp = i[j - 1]
-                        if(tmp[-1] != '.'):
-                            tmp = i[j]
-                            if(tmp[-1] == '.' or tmp[-1] == ','):
-                                """remove last . simbols than add in dictionary"""
-                                tmp = tmp[0:(len(tmp) - 1)]
-                                my_dict[tmp.upper()] = 1
-                                
-                            else:
-                                my_dict[i[j].upper()] = 1
+"""Delete all simboles besides letters and numbers"""
+def pars_text(strings):
+    for i in range(0, len(strings)):
+        for j in range(0, len(strings[i])):
+            strings[i][j] = re.sub(r'[\W_]+', '', strings[i][j])
 
 
-def popular_word(my_dict):
+"""if word is popular add set, when line first word popular that word add other set"""
+def string_set(strings, all_popular_word, line_first_popular_word):
+    for i in range(0, len(strings)):
+        print(i, strings[i])
+        if(i == 0 and is_popular(strings[i])):
+            line_first_popular_word.add(strings[i].upper())
+        elif(is_popular(strings[i]) and (not strings[i].upper() in line_first_popular_word)):
+            all_popular_word.add(strings[i].upper())
+
+
+"""
+ check word is popular?
+ popular word can be all uppercase or first uppercase rest part letters: Asdf ASDF
+ popular word cant be ASaa AFF4 aaa2 aaa
+"""
+def is_popular(string):
+    if((not bool(re.search(r'\d', string))) and (string[0].isupper() and string[1:].islower() )):
+        return True
+    elif(not bool(re.search(r'\d', string)) and string.isupper()):
+        return True
+    return False
+
+
+"""counting popular word repeat count """
+def count_of_popular_word(popular_word_count, all_popular_words, line_first_popular_words):
+    for i in all_popular_words:
+        for j in i:
+            if(j in popular_word_count) :
+                popular_word_count[j] = popular_word_count[j] + 1
+            else:
+                popular_word_count[j] = 1
+
+    for i in line_first_popular_words:
+        for j in i:
+            if(j in popular_word_count) :
+                popular_word_count[j] = popular_word_count[j] + 1
+
+
+"""print popular words"""
+def print_popular_word(count , popular_word_count):
     print("\nThe Popular words is: \n")
-    if(len(my_dict) < 3):
-        for i in my_dict:
-            print(i, ":", my_dict[i])
+    if(len(popular_word_count) < count):
+        for i in popular_word_count:
+            print(i, ":", popular_word_count[i])
     else:
-        my_dict = sorted(my_dict.items(), key=lambda x: x[1], reverse=True)
-        for i in range(0, 3):
-            print(my_dict[i][0], ":", my_dict[i][1])
-
+        popular_word_count = sorted(popular_word_count.items(), key=lambda x: x[1], reverse=True)
+        for i in range(0, count):
+            print(popular_word_count[i][0], ":", popular_word_count[i][1])
 
 def main():
-    n = 0
-    n = get_line_count()
+    n = get_line_count("Enter Number of lines: ")
 
+    """texts"""
     strings = []
     input_lines(n, strings)
-    
-    my_dict = {}
-    add_popular_word(strings, my_dict)
 
-    popular_word(my_dict)
+    count = get_line_count("How many popular word du you wont find: ")
+    pars_text(strings)
 
+    """popular words"""
+    line_first_popular_words = []
+    all_popular_words = []
+    for i in strings:
+        all_popular_word = set('')
+        line_first_popular_word = set('')
+        string_set(i, all_popular_word, line_first_popular_word)
+        line_first_popular_words.append(list(line_first_popular_word))
+        all_popular_words.append(list(all_popular_word))
+
+    """conting popular words"""
+    popular_word_count = dict()
+    count_of_popular_word(popular_word_count, all_popular_words, line_first_popular_words)
+
+    """print popular words"""
+    print_popular_word(count, popular_word_count)
 
 if __name__ == "__main__":
     main()
