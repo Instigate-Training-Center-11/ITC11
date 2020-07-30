3f8d561880f9aef1163def61e15b608d44ac9f7e
Armine <arm.gevorgyan94@gmail.com>
2020-07-30 21:53:42 +0400

    -Modified Comands.txt

--- DataBases/Online_Market/Online_Market.sql
+++ DataBases/Online_Market/Online_Market.sql
@@ -29,7 +29,7 @@ CREATE TABLE `Costumers` (
   `street` varchar(50) NOT NULL,
   `phone` varchar(35) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -38,9 +38,43 @@ CREATE TABLE `Costumers` (
 
 LOCK TABLES `Costumers` WRITE;
 /*!40000 ALTER TABLE `Costumers` DISABLE KEYS */;
-INSERT INTO `Costumers` VALUES (1,'Karine','Vanadzor','Bazum','37498526247'),(2,'Gayane','Gyumri','Dimac','37494223344'),(3,'Hrant','Spitak','Sari_tax','37477854199'),(4,'Memua','Ashtarak','1tax','37455669988');
+INSERT INTO `Costumers` VALUES (1,'Karine','Vanadzor','Bazum','37498526247'),(2,'Gayane','Gyumri','Dimac','37494223344'),(3,'Hrant','Spitak','Sari_tax','37477854199'),(5,'Anna','Ararat','Erebuni','05566447788'),(6,'Vanna','Aparan','Kentron','885556699'),(8,'VVVVVanna','Aparan','Kentron','885556699'),(9,'A66666AAAnna','Ararat','Erebuni','05566447788'),(10,'VVVVVa55555nna','Aparan','Kentron','885556699'),(11,'A66666AAAnna','Ararat','Erebuni','05566447788'),(12,'VVVVVa55555nna','Aparan','Kentron','885556699');
 /*!40000 ALTER TABLE `Costumers` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER ins_sum BEFORE INSERT ON Costumers
+       FOR EACH ROW SET @sum = @sum + 1 */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER ins_sum1 BEFORE INSERT ON Costumers        FOR EACH row  begin if @sum < 3 then set @sum = @sum + 1;
+else set @sum = 0;
+end if;
+end */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `OrderLines`
@@ -52,15 +86,17 @@ DROP TABLE IF EXISTS `OrderLines`;
 CREATE TABLE `OrderLines` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `product_id` int(11) NOT NULL,
+  `order_id` int(11) NOT NULL,
   `productQuantity` int(11) NOT NULL,
-  `totalSale` int(11) NOT NULL,
   `productDelivered` tinyint(1) NOT NULL,
   `productDeliveredDate` date NOT NULL,
   `productDeliveredTime` datetime NOT NULL,
   PRIMARY KEY (`id`),
   KEY `product_id` (`product_id`),
-  CONSTRAINT `OrderLines_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Products` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
+  KEY `order_id` (`order_id`),
+  CONSTRAINT `OrderLines_ibfk_1` FOREIGN KEY (`product_id`) REFERENCES `Products` (`id`),
+  CONSTRAINT `OrderLines_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `Orders` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -69,7 +105,7 @@ CREATE TABLE `OrderLines` (
 
 LOCK TABLES `OrderLines` WRITE;
 /*!40000 ALTER TABLE `OrderLines` DISABLE KEYS */;
-INSERT INTO `OrderLines` VALUES (1,1,3,96000,1,'2020-04-24','2020-04-24 15:26:32'),(2,2,2,86000,1,'2020-06-08','2020-06-08 20:05:32'),(3,3,4,2800,1,'2020-07-12','2020-07-12 12:00:32'),(4,5,2,35600,1,'2020-06-05','2020-06-05 13:30:32'),(5,4,1,480000,1,'2020-07-27','2020-07-27 19:10:12'),(6,4,1,480000,1,'2020-07-27','2020-07-27 19:10:12'),(7,3,10,7000,1,'2020-07-22','2020-07-22 14:50:27');
+INSERT INTO `OrderLines` VALUES (1,1,1,5,0,'2020-06-29','2020-06-29 16:45:25'),(2,3,1,4,0,'2020-06-29','2020-06-29 16:45:25'),(3,2,2,4,1,'2020-06-28','2020-06-28 22:45:25'),(4,4,2,6,1,'2020-06-28','2020-06-28 22:45:25');
 /*!40000 ALTER TABLE `OrderLines` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -88,12 +124,8 @@ CREATE TABLE `Orders` (
   `orderCompleted` tinyint(1) NOT NULL,
   `orderCompletedDate` date NOT NULL,
   `orderComplitedTime` datetime NOT NULL,
-  `orderLines_id` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `costumer_id` (`costumer_id`),
-  KEY `orderLines_id` (`orderLines_id`),
-  CONSTRAINT `Orders_ibfk_1` FOREIGN KEY (`costumer_id`) REFERENCES `Costumers` (`id`),
-  CONSTRAINT `Orders_ibfk_2` FOREIGN KEY (`orderLines_id`) REFERENCES `OrderLines` (`id`)
+  KEY `costumer_id` (`costumer_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -103,7 +135,7 @@ CREATE TABLE `Orders` (
 
 LOCK TABLES `Orders` WRITE;
 /*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
-INSERT INTO `Orders` VALUES (1,1,'2020-03-10','2020-03-10 21:25:36',1,'2020-04-22','2020-04-22 14:54:22',2),(2,3,'2020-05-12','2020-05-12 11:44:36',1,'2020-06-01','2020-06-01 18:22:17',3),(3,2,'2020-05-24','2020-05-24 17:05:48',1,'2020-06-02','2020-06-02 20:35:29',4),(4,4,'2020-06-29','2020-06-29 22:17:44',1,'2020-07-10','2020-07-10 15:22:29',1);
+INSERT INTO `Orders` VALUES (1,1,'2020-03-10','2020-03-10 21:25:36',1,'2020-04-22','2020-04-22 14:54:22'),(2,3,'2020-05-12','2020-05-12 11:44:36',1,'2020-06-01','2020-06-01 18:22:17'),(3,2,'2020-05-24','2020-05-24 17:05:48',1,'2020-06-02','2020-06-02 20:35:29'),(4,4,'2020-06-29','2020-06-29 22:17:44',1,'2020-07-10','2020-07-10 15:22:29');
 /*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -124,7 +156,7 @@ CREATE TABLE `Products` (
   PRIMARY KEY (`id`),
   KEY `suppliert_id` (`suppliert_id`),
   CONSTRAINT `Products_ibfk_1` FOREIGN KEY (`suppliert_id`) REFERENCES `Supplierts` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -133,7 +165,7 @@ CREATE TABLE `Products` (
 
 LOCK TABLES `Products` WRITE;
 /*!40000 ALTER TABLE `Products` DISABLE KEYS */;
-INSERT INTO `Products` VALUES (1,'Table',25000,32000,36,1),(2,'Door',37000,43000,57,2),(3,'Cup',500,700,157,3),(4,'Phone',44000,480000,65,4),(5,'Clock',12500,17800,120,4);
+INSERT INTO `Products` VALUES (1,'Table',25000,32000,36,1),(2,'Door',37000,43000,57,2),(3,'Cup',500,700,157,3),(4,'Phone',44000,480000,65,4),(5,'Clock',12500,17800,120,4),(6,'Lack',2000,2500,0,2);
 /*!40000 ALTER TABLE `Products` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -173,4 +205,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-07-29  2:28:39
+-- Dump completed on 2020-07-30 21:51:46
