ca4338a1cba6756034d802e93d5c5c2894560a0d
Mane Antonyan <45038600+mKolibri@users.noreply.github.com>
2020-07-31 02:34:48 +0400

    Delete deleteConnection.sql

--- mySQL/day 1-2/deleteConnection.sql
+++
@@ -1,5 +0,0 @@
--- Delete from table where exists primary key
--- Automaticaly delete every row where exists customer ID
-CREATE TRIGGER deleteConnections AFTER DELETE ON orders
-FOR EACH ROW
-DELETE FROM orderLines WHERE orderID = old.orderID;
