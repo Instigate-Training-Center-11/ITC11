1ea1f2188060c1bf2f592ea8b327d821232b4ede
mkolibri <maneantonyan@gmail.com>
2020-07-30 14:47:27 +0400

    Updates, minor fixes

--- mySQL/deleteConnection.sql
+++ mySQL/deleteConnection.sql
@@ -1,18 +1,6 @@
+--- First way ---
 -- Delete from table where exists primary key
 -- Automaticaly delete every row where exists customer ID
-CREATE TRIGGER deleteConnections AFTER DELETE ON customers
+CREATE TRIGGER deleteConnections AFTER DELETE ON orders
 FOR EACH ROW
-DELETE FROM orders WHERE customerID = old.customerID;
-
--- For deleting row from order
--- To fix allow error: " a foreign key constraint fails"
-
--- Turn off foregn key checking and delete row
-CREATE TRIGGER deleteBefore BEFORE DELETE ON customers
-FOR EACH ROW
-SET foreign_key_checks = 0;
-
--- Turn on foregn key checking after deleting rows
-CREATE TRIGGER deleteAfter AFTER DELETE ON customers
-FOR EACH ROW
-SET foreign_key_checks = 1;
+DELETE FROM orderLines WHERE orderID = old.orderID;
