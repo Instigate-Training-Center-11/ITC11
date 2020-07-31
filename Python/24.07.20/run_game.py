4d7033c75a578a648d65ac63774cd698cef58f16
Artashes <arto1993@github.com>
2020-08-01 02:17:10 +0400

    Fix chess

--- Python/24.07.20/run_game.py
+++ Python/24.07.20/run_game.py
@@ -1,8 +1,9 @@
-import figure
-import board
+from figure import *
+from board import *
 import string
 import os
 
+# Check moves for different figures
 def check_move(current_white_figure,current_black_figure, new_x, new_y, cordinate_x, cordinate_y, count):
     for i in range(0, count):
         black_x = current_black_figure[i].get_x()
@@ -12,8 +13,8 @@ def check_move(current_white_figure,current_black_figure, new_x, new_y, cordinat
         if black_x == cordinate_x and black_y == cordinate_y:
                 if current_black_figure[i].safe_move(new_x, new_y):
                     current_black_figure[i].set_x_y(new_x, new_y)
-                    board.move(new_board.board, black_x, black_y, ' ')
-                    board.move(new_board.board, new_x, new_y, current_black_figure[i].get_simvol())
+                    move(new_board.board, black_x, black_y, ' ')
+                    move(new_board.board, new_x, new_y, current_black_figure[i].get_simvol())
                     print("Nice move for:", end = " ")
                     print(current_black_figure[i].get_simvol())
                 else:
@@ -22,18 +23,18 @@ def check_move(current_white_figure,current_black_figure, new_x, new_y, cordinat
         elif white_x == cordinate_x and white_y == cordinate_y:
                 if current_white_figure[i].safe_move(new_x, new_y):
                     current_white_figure[i].set_x_y(new_x, new_y)
-                    board.move(new_board.board, white_x, white_y, ' ')
-                    board.move(new_board.board, new_x, new_y, current_white_figure[i].get_simvol())
+                    move(new_board.board, white_x, white_y, ' ')
+                    move(new_board.board, new_x, new_y, current_white_figure[i].get_simvol())
                     print("Nice move for:", end = " ")
                     print(current_white_figure[i].get_simvol())
                 else:
                     print("No such move for:", end = " ")
                     print(current_white_figure[i].get_simvol())
 
-new_board = board.Board()
+new_board = Board()
 
 #|♟|♞|♝|♜|♛|♚|♙|♘|♗|♖|♕|♔|
-
+# Create lists for chess figure objects
 black_pown = []
 white_pown = []
 black_rook = []
@@ -47,49 +48,51 @@ white_king = []
 black_queen = []
 white_queen = []
 
+# Painting figures in the board
 for i in range(0, 8):
-    black_pown.append(figure.Pown(1, i, "♟"))
-    board.move(new_board.board, 1, i, "♟")
-    white_pown.append(figure.Pown(6, i, "♙"))
-    board.move(new_board.board, 6, i, "♙")
+    black_pown.append(Pown(1, i, "♟"))
+    move(new_board.board, 1, i, "♟")
+    white_pown.append(Pown(6, i, "♙"))
+    move(new_board.board, 6, i, "♙")
 for i in range(0, 8, 7):
-    black_rook.append(figure.Rook(0, i, "♜"))
-    board.move(new_board.board, 0, i, "♜")
-    white_rook.append(figure.Rook(7, i, "♖"))
-    board.move(new_board.board, 7, i, "♖")
+    black_rook.append(Rook(0, i, "♜"))
+    move(new_board.board, 0, i, "♜")
+    white_rook.append(Rook(7, i, "♖"))
+    move(new_board.board, 7, i, "♖")
 for i in range(1, 8, 5):
-    black_knight.append(figure.Knight(0, i, "♞"))
-    board.move(new_board.board, 0, i, "♞")
-    white_knight.append(figure.Knight(7, i, "♘"))
-    board.move(new_board.board, 7, i, "♘")
+    black_knight.append(Knight(0, i, "♞"))
+    move(new_board.board, 0, i, "♞")
+    white_knight.append(Knight(7, i, "♘"))
+    move(new_board.board, 7, i, "♘")
 for i in range(2, 8, 3):
-    black_bishop.append(figure.Bishop(0, i, "♝"))
-    board.move(new_board.board, 0, i, "♝")
-    white_bishop.append(figure.Bishop(7, i, "♗"))
-    board.move(new_board.board, 7, i, "♗")
+    black_bishop.append(Bishop(0, i, "♝"))
+    move(new_board.board, 0, i, "♝")
+    white_bishop.append(Bishop(7, i, "♗"))
+    move(new_board.board, 7, i, "♗")
 
-black_king.append(figure.King(0, 3, "♛"))
-board.move(new_board.board, 0, 3, "♛")
-white_king.append(figure.King(7, 4, "♕"))
-board.move(new_board.board, 7, 4, "♕")
+black_king.append(King(0, 3, "♛"))
+move(new_board.board, 0, 3, "♛")
+white_king.append(King(7, 4, "♕"))
+move(new_board.board, 7, 4, "♕")
 
-black_queen.append(figure.Queen(0, 4, "♚"))
-board.move(new_board.board, 0, 4, "♚")
-white_queen.append(figure.Queen(7, 3, "♔"))
-board.move(new_board.board, 7, 3, "♔")
+black_queen.append(Queen(0, 4, "♚"))
+move(new_board.board, 0, 4, "♚")
+white_queen.append(Queen(7, 3, "♔"))
+move(new_board.board, 7, 3, "♔")
 
+# Start game
 is_exit = False
 os.system('cls' if os.name == 'nt' else 'clear')
 print("Good luck!!!")
 while is_exit == False:
-    board.print_board(new_board.board)
+    print_board(new_board.board)
     print("Please insert some figure cordinates.(Example: A and 2)")
     cordinate_y = ord(input("First cordinate: ")) - 65
     cordinate_x = int(input("Second cordinate: ")) - 1
     new_y = ord(input("New first cordinate: ")) - 65
     new_x = int(input("New second cordinate: ")) - 1
 
-    simvol = board.check(new_board.board, cordinate_x, cordinate_y)
+    simvol = check(new_board.board, cordinate_x, cordinate_y)
     os.system('cls' if os.name == 'nt' else 'clear')
     if simvol == '.':
         print("Empty")
