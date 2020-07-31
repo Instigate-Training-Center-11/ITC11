32e304fdbf693f53057b500abd6632892e37715f
Anna Harutyunyan <anna.bk85@mail.ru>
2020-07-31 18:37:34 +0400

    Updated homework

---
+++ Db/30_07_2020/proc_trigg_join.sql
@@ -0,0 +1,158 @@
+-- MySQL dump 10.13  Distrib 5.7.30, for Linux (x86_64)
+--
+-- Host: localhost    Database: homework
+-- ------------------------------------------------------
+-- Server version	5.7.30-0ubuntu0.16.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Customers`
+--
+
+DROP TABLE IF EXISTS `Customers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Customers` (
+  `Customer_Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Surname` varchar(255) NOT NULL,
+  `Phone` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`Customer_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Customers`
+--
+
+LOCK TABLES `Customers` WRITE;
+/*!40000 ALTER TABLE `Customers` DISABLE KEYS */;
+INSERT INTO `Customers` VALUES (1,'Johnson','Smith','123456'),(2,'Davis','Williams','147852'),(3,'Jensen','Ross','654123');
+/*!40000 ALTER TABLE `Customers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `OrderLines`
+--
+
+DROP TABLE IF EXISTS `OrderLines`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `OrderLines` (
+  `Order_Id` int(10) unsigned NOT NULL,
+  `Count` int(10) unsigned DEFAULT NULL,
+  `Product_Id` int(10) unsigned DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `OrderLines`
+--
+
+LOCK TABLES `OrderLines` WRITE;
+/*!40000 ALTER TABLE `OrderLines` DISABLE KEYS */;
+INSERT INTO `OrderLines` VALUES (1,14,1),(4,44,1),(4,30,2),(2,300,3),(5,302,4),(5,2,5);
+/*!40000 ALTER TABLE `OrderLines` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Orders`
+--
+
+DROP TABLE IF EXISTS `Orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Orders` (
+  `Order_Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Customer_Id` int(10) unsigned DEFAULT NULL,
+  `Total_count` int(10) unsigned DEFAULT NULL,
+  `Delivery_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `bool` tinyint(1) NOT NULL,
+  PRIMARY KEY (`Order_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Orders`
+--
+
+LOCK TABLES `Orders` WRITE;
+/*!40000 ALTER TABLE `Orders` DISABLE KEYS */;
+INSERT INTO `Orders` VALUES (1,1,55,'2020-07-27 19:01:13',0),(2,3,1000,'2020-07-27 19:09:05',0),(3,3,120,'2020-07-27 19:09:17',0),(4,1,20,'2020-07-27 19:09:24',0),(5,2,420,'2020-07-27 19:09:33',0);
+/*!40000 ALTER TABLE `Orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Products`
+--
+
+DROP TABLE IF EXISTS `Products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Products` (
+  `Product_Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Suppliers_Id` int(10) unsigned NOT NULL,
+  `Product_name` varchar(255) NOT NULL,
+  `Shelf_life` varchar(255) NOT NULL,
+  `Price` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`Product_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Products`
+--
+
+LOCK TABLES `Products` WRITE;
+/*!40000 ALTER TABLE `Products` DISABLE KEYS */;
+INSERT INTO `Products` VALUES (1,1,'Milk','2 days',450),(2,1,'Honey','1 year',5000),(3,2,'Cheese','10 days',3000),(4,2,'Tea','6 mounts',2000),(5,3,'Coffee','1 mounts',4000);
+/*!40000 ALTER TABLE `Products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Suppliers`
+--
+
+DROP TABLE IF EXISTS `Suppliers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Suppliers` (
+  `Suppliers_Id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `Name` varchar(255) NOT NULL,
+  `Surname` varchar(255) NOT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `Phone` varchar(255) NOT NULL,
+  PRIMARY KEY (`Suppliers_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Suppliers`
+--
+
+LOCK TABLES `Suppliers` WRITE;
+/*!40000 ALTER TABLE `Suppliers` DISABLE KEYS */;
+INSERT INTO `Suppliers` VALUES (1,'Robert','Thomas','xxx Street','423651'),(2,'Tom','Bradley','yyy Street','731954'),(3,'George','Thomas','zzz Street','457100');
+/*!40000 ALTER TABLE `Suppliers` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-07-31  2:24:40
