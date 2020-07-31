ee31d41208881797793d7f04275c4dad34632b77
Artashes <arto1993@github.com>
2020-07-31 18:08:04 +0400

    UPDATE CHESS

--- Python/24.07.20/run_game.py
+++ Python/24.07.20/run_game.py
@@ -2,6 +2,21 @@ import figure
 import board
 import string
 
+def check_move(current_white_figure,current_black_figure, count):
+    for i in range(0, count):
+        black_x = current_white_figure[i].get_x()
+        black_y = current_white_figure[i].get_y()
+        white_x = current_black_figure[i].get_x()
+        white_y = current_black_figure[i].get_y()
+        if black_x == cordinate_x and black_y == cordinate_y:
+            current_white_figure[i].set_x_y(new_x, new_y)
+            board.move(new_board.board, black_x, black_y, '.')
+            board.move(new_board.board, new_x, new_y, current_white_figure[i].get_simvol())
+        elif white_x == cordinate_x and white_y == cordinate_y:
+                current_black_figure[i].set_x_y(new_x, new_y)
+                board.move(new_board.board, white_x, white_y, '.')
+                board.move(new_board.board, new_x, new_y, current_black_figure[i].get_simvol())
+
 new_board = board.Board()
 
 #|♟|♞|♝|♜|♛|♚|♙|♘|♗|♖|♕|♔|
@@ -14,6 +29,10 @@ black_knight = []
 white_knight = []
 black_bishop = []
 white_bishop = []
+black_king = []
+white_king = []
+black_queen = []
+white_queen = []
 
 for i in range(0, 8):
     black_pown.append(figure.Figure(1, i, "♟"))
@@ -36,14 +55,14 @@ for i in range(2, 8, 3):
     white_bishop.append(figure.Figure(7, i, "♗"))
     board.move(new_board.board, 7, i, "♗")
 
-black_king = figure.Figure(0, 3, "♛")
+black_king.append(figure.Figure(0, 3, "♛"))
 board.move(new_board.board, 0, 3, "♛")
-white_king = figure.Figure(7, 4, "♕")
+white_king.append(figure.Figure(7, 4, "♕"))
 board.move(new_board.board, 7, 4, "♕")
 
-black_queen = figure.Figure(0, 4, "♚")
+black_queen.append(figure.Figure(0, 4, "♚"))
 board.move(new_board.board, 0, 4, "♚")
-white_queen = figure.Figure(7, 3, "♔")
+white_queen.append(figure.Figure(7, 3, "♔"))
 board.move(new_board.board, 7, 3, "♔")
 
 is_exit = False
@@ -62,20 +81,16 @@ while is_exit == False:
         print("Empty")
     else:
         if simvol == '♟' or simvol == '♙':
-            for i in range(0, 8):
-                black_x = black_pown[i].get_x()
-                black_y = black_pown[i].get_y()
-                white_x = white_pown[i].get_x()
-                white_y = white_pown[i].get_y()
-                if black_x == cordinate_x and black_y == cordinate_y:
-                    black_pown[i].set_x_y(new_x, new_y)
-                    board.move(new_board.board, black_x, black_y, '.')
-                    board.move(new_board.board, new_x, new_y, black_pown[i].get_simvol())
-                elif white_x == cordinate_x and white_y == cordinate_y:
-                    white_pown[i].set_x_y(new_x, new_y)
-                    board.move(new_board.board, white_x, white_y, '.')
-                    board.move(new_board.board, new_x, new_y, white_pown[i].get_simvol())
-
+            check_move(black_pown, white_pown, 8)
+        elif simvol == '♜' or simvol == '♖':
+            check_move(black_rook, white_rook, 2)
+        elif simvol == '♞' or simvol == '♘':
+            check_move(black_knight, white_knight, 2)
+        elif simvol == '♝' or simvol == '♗':
+            check_move(black_bishop, white_bishop, 2)
+        elif simvol == '♛' or simvol == '♕':
+            check_move(black_king, white_king, 1)
+        elif simvol == '♚' or simvol == '♔':
+            check_move(black_queen, white_queen, 1)
 
     ##TO_DO
-
