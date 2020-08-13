0e365be1e630df13a06317f6261e129ddd9eac1b
mkolibri <maneantonyan@gmail.com>
2020-08-13 17:11:20 +0400

    Added today's classwork

--- web/JavaScript/classwork/graphic/index.js
+++ web/JavaScript/classwork/graphic/index.js
@@ -1,49 +1,33 @@
-const tasks = {};
+const getRandomColor = function() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
-
-// 1. Need to get all data using "Promises.all" function.
-// ... You can use fetch for example
-tasks.taskOne = function() {
-    const urls = [
-        'https://raw.githubusercontent.com/Instigate-Training-Center-11/ITC11/Mane_Antonyan/web/JavaScript/homework/id.json',
-        'https://raw.githubusercontent.com/Instigate-Training-Center-11/ITC11/Mane_Antonyan/web/JavaScript/homework/users.json',
-    ];
-
-    // Recreate every URL to promise, returned fetch
-    let requests = urls.map(url => fetch(url), {
-        credentials: 'same-origin'
-    });
-
-    // Promise.all will wait for done all promises
-    Promise.all(requests)
-    .then(responses => responses.forEach(response => console.log(response.json())))
-    .catch(error => alert("Something went wrong in 1st function!"));
-
+const createGraphic = async function() {
+    const url = 'https://raw.githubusercontent.com/Instigate-Training-Center-11/ITC11/Mane_Antonyan/web/JavaScript/classwork/graphic/id.json';
+    let data = await fetch(url).then(data => data.json());
+    paintGraphic(data, "main");
 };
 
-
-// 2. Get rows from investment data in which investments are not less then 500
-// and then get the that people's info from people's data and combine.
-tasks.taskTwo = function() {
-    const url = 'https://raw.githubusercontent.com/Instigate-Training-Center-11/ITC11/Mane_Antonyan/web/JavaScript/homework/id.json';
-
-    // Recreate every URL to promise, returned fetch, parse from json to js object
-    let requests = urls.map(url => fetch(url).then(data => data.json()));
-
-    Promise.all(requests)
-    .then(data => {
-        ids = data[0].investmentsData;
-        users = data[1].peoplesData;
-        let filtered = ids.filter(element => element.investments >= 500);
-        const allData = filtered.map(element => {
-            let id = element.id;
-            user = users.find(user => user.id === id);
-            return Object.assign(user, element);
-        });
-        console.log(allData);
-    })
-    .catch((err) => {
-        alert("Something went wrong in 2nd function!");
-        console.log(err.status);
+const paintGraphic = function(data, id) {
+    const allData = data.investmentsData;
+    const target = document.getElementById(id);
+    console.log(allData);
+    allData.forEach(elem => {
+        console.log(elem)
+        let tag = document.createElement('div');
+        tag.id = elem.id;
+        tag.onclick = function() {
+            document.getElementById("id").innerHTML = "id : " + elem.id;
+            document.getElementById("investment").innerHTML = "investment : " + elem.investments;
+        };
+        tag.style.cssText = "background-color:" + getRandomColor() +
+            "; height:" + elem.investments + '%; width: 50px;';
+        target.appendChild(tag);
     });
-};
+    addEvents(allData);
+}
