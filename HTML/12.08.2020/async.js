18edc8228fdade81e4f54bf1822717b72a9c0129
Armine <arm.gevorgyan94@gmail.com>
2020-08-14 10:42:12 +0400

    -Modified async.js change style

--- HTML/12.08.2020/async.js
+++ HTML/12.08.2020/async.js
@@ -8,7 +8,6 @@ let getData2 = async function() {
     let res1 = await fetch('https://raw.githubusercontent.com/Instigate-Training-Center-11/ITC11/Armine_Gevorgyan/HTML/12.08.2020/file2.json');
     let data = await res1.json();
     return data;
-
 }
 
 Promise.all([getData1(), getData2()]).then(values => {
@@ -23,56 +22,51 @@ function addElements(id, data) {
             }
         }
     }
-
 }
-
-// getData();
 
 function createData(data, i, j) {
     let content = document.createElement('DIV');
-                content.id = 'content_' + i + 1;
-                let imageDiv = document.createElement('DIV');
-                let infoDiv = document.createElement('DIV');
-                infoDiv.className = 'info';
-                let img = document.createElement('IMG');
-                let info = document.createElement('DIV');
-                let info_1 = document.createElement('DIV');
-                let info_2 = document.createElement('DIV');
-                let rBin = document.createElement('IMG');
+    content.id = 'content_' + i + 1;
+    let imageDiv = document.createElement('DIV');
+    let infoDiv = document.createElement('DIV');
+    infoDiv.className = 'info';
+    let img = document.createElement('IMG');
+    let info = document.createElement('DIV');
+    let info_1 = document.createElement('DIV');
+    let info_2 = document.createElement('DIV');
+    let rBin = document.createElement('IMG');
+    let name = document.createElement('H2');
+    let lname = document.createElement('H2');
+    let age = document.createElement('H3');
+    let job = document.createElement('H3');
+    let investments = document.createElement('H3');
+    let investmentsData = document.createElement('H3');
 
-                let name = document.createElement('H2');
-                let lname = document.createElement('H2');
-                let age = document.createElement('H3');
-                let job = document.createElement('H3');
-                let investments = document.createElement('H3');
-                let investmentsData = document.createElement('H3');
+    name.innerText = data[1][j].name;
+    lname.innerText = data[1][j].lname;
+    age.innerText = data[1][j].age;
+    job.innerText = data[1][j].job;
+    investments.innerText = 'Investments: ';
+    investmentsData.innerText = data[0][i].investments;
+    rBin.className = 'rBin';
+    rBin.src = './rBin.png';
+    rBin.addEventListener('click', function(){
+        content.style.display = 'none';
+    })
+    img.src = data[1][j].img;
 
-                name.innerText = data[1][j].name;
-                lname.innerText = data[1][j].lname;
-                age.innerText = data[1][j].age;
-                job.innerText = data[1][j].job;
-                investments.innerText = 'Investments: ';
-                investmentsData.innerText = data[0][i].investments;
-                rBin.className = 'rBin';
-                rBin.src = './rBin.png';
-                rBin.addEventListener('click', function(){
-                    content.style.display = 'none';
-                })
-
-                console.log(data[1][j]);
-                img.src = data[1][j].img;
-                document.getElementById('main').appendChild(content);
-                content.appendChild(imageDiv);
-                content.appendChild(infoDiv);
-                imageDiv.appendChild(img);
-                info.appendChild(name);
-                info.appendChild(lname);
-                info.appendChild(rBin);
-                info_1.appendChild(age);
-                info_1.appendChild(job);
-                info_2.appendChild(investments);
-                info_2.appendChild(investmentsData);
-                infoDiv.appendChild(info);
-                infoDiv.appendChild(info_1);
-                infoDiv.appendChild(info_2);
+    document.getElementById('main').appendChild(content);
+    content.appendChild(imageDiv);
+    content.appendChild(infoDiv);
+    imageDiv.appendChild(img);
+    info.appendChild(name);
+    info.appendChild(lname);
+    info.appendChild(rBin);
+    info_1.appendChild(age);
+    info_1.appendChild(job);
+    info_2.appendChild(investments);
+    info_2.appendChild(investmentsData);
+    infoDiv.appendChild(info);
+    infoDiv.appendChild(info_1);
+    infoDiv.appendChild(info_2);
 }
