2e3f3a79fb83f4f6e5a4b4e56d02a99eafde3e04
ArmenuhiKocharyan <armamik@gmail.com>
2020-08-16 21:20:26 +0400

    Added homework about getting inputing datas

---
+++ Web/JavaScript/getInput/script.js
@@ -0,0 +1,15 @@
+function myFunction() {
+    var x = document.getElementById("form_name").value;
+    document.getElementById("demo").innerHTML = x;
+    console.log(x);
+    var x = document.getElementById("label_name").value;
+    document.getElementById("demo1").innerHTML = x;
+    console.log(x);
+    var x = document.getElementById("email").value;
+    document.getElementById("demo2").innerHTML = x;
+    console.log(x);
+    var x = document.getElementById("birthday").value;
+    document.getElementById("demo3").innerHTML = x;
+    console.log(x);
+}
+ 
