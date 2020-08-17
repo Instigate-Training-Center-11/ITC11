db6c0b08014eae1655ccb5e564dfd681735b2548
ArmenuhiKocharyan <armamik@gmail.com>
2020-08-17 22:33:30 +0400

    Initial version

--- Web/node/server.js
+++ Web/node/server.js
@@ -1,22 +1,15 @@
-var data =require('./firstfile.json');
-
+var data = require('./firstfile.json');
 var http = require('http');
 var url = require('url');
-
 var server = new http.Server();
 server.listen(8080, '93.185.45.82');
-
-server.on('request', function(req, res) {
+server.on('request', function (req, res) {
     console.log(req.headers);
-    
     var reqUrl = url.parse(req.url, true);
     console.log(reqUrl.query);
-
-    const {name, age} = reqURL.query;
-
-    const resData = data.peoplesData.find(item=>item.name === name && item.age === parseInt(age));
-
-    if(resData) {
+    const { name, age } = reqURL.query;
+    const resData = data.peoplesData.find(item => item.name === name && item.age === parseInt(age));
+    if (resData) {
         res.end(JSON.stringify(resData))
     } else {
         res.statusCode = 404;
@@ -24,4 +17,3 @@ server.on('request', function(req, res) {
     }
 })
 
- 
