217900084f76ca653407d01fb8707ecd43737f9b
Artashes <arto1993@github.com>
2020-08-20 11:25:49 +0400

    Node_JS homework

--- Web/Node_JS/server.js
+++ Web/Node_JS/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome..." });
 });
 
 const PORT = process.env.PORT || 3000;
