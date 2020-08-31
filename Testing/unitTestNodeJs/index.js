5d1a94eb6e8edea2408484a68323efaec7838aaa
Armine <arm.gevorgyan94@gmail.com>
2020-08-31 17:07:34 +0400

    -Modified index.js

--- Testing/unitTestNodeJs/index.js
+++ Testing/unitTestNodeJs/index.js
@@ -1,63 +1,80 @@
 const add = function(num1, num2) {
-    return num1 + num2;
+    try {
+        return parseFloat(num1) + parseFloat(num2);
+    } catch (error) {
+        return "Not a Number";
+    }
 }
 
 const sub = function(num1, num2) {
-    return num1 - num2;
+    try {
+        return parseFloat(num1) - parseFloat(num2);
+    } catch (error) {
+        return "Not a Number";
+    }
 }
 
 const mul = function(num1, num2) {
-    return num1 * num2;
+    try {
+        return parseFloat(num1) * parseFloat(num2);
+    } catch (error) {
+        return "Not a Number";
+    }
 }
 
 const div = function(num1, num2) {
-    if(num2 !== 0) {
-        return num1 / num2;
+    try {
+        if(parseFloat(num2) !== 0) {
+            return parseFloat(num1) / parseFloat(num2);
+        }
+
+        return "Can not division by 0";
+    } catch (error) {
+        return "Not a Number";
     }
 
-    return "Can not division by 0";
 }
 
-const testForAdd = function() {
-    const result = add(3, 5);
-    if(8 === result) {
+const testForAdd = function(val1, val2, res) {
+    const result = add(val1, val2);
+    if(res == result || result === "Not a Number") {
         console.log("Functional test for add function is ready");
     } else {
-        console.log("Functional test for add function is filed");
+        console.log("Functional test for add function is fail");
     }
 }
 
-const testForSub = function() {
-    const result = sub(5, 8);
+const testForSub = function(val1, val2, res) {
+    const result = sub(val1, val2);
 
-    if(-3 === result) {
+    if(res == result || result === "Not a Number") {
         console.log("Functional test for subscription function is ready");
     } else {
-        console.log("Functional test for subscription function is filed");
+        console.log("Functional test for subscription function is fail");
     }
 }
 
-const testForMul = function() {
-    const result = mul(4, 8);
+const testForMul = function(val1, val2, res) {
+    const result = mul(val1, val2);
 
-    if(32 === result) {
+    if(res == result || result === "Not a Number") {
         console.log("Functional test for multiplication function is ready");
     } else {
-        console.log("Functional test for multiplication function is filed");
+        console.log("Functional test for multiplication function is fail");
     }
 }
 
-const testForDiv = function() {
-    const result = div(12, 0);
+const testForDiv = function(val1, val2, res) {
+    const result = div(val1, val2);
 
-    if(12/0 === result) {
+    if(res == result || result === "Not a Number" || result === "Can not division by 0") {
         console.log("Functional test for division function is ready");
     } else {
-        console.log("Functional test for division function is filed");
+        console.log("Functional test for division function is fail");
     }
 }
 
-testForAdd();
-testForDiv();
-testForMul();
-testForSub();
+testForAdd('4', 5, 'a');
+testForDiv(8, '0');
+testForMul('4', 5, 'aaa');
+testForSub(32, 24, 8);
