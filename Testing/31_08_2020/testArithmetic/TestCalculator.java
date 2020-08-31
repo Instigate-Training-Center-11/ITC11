6430be569bf1666693ffb0607c8461632197590e
Unanyan <meruzhan.unanyan@bk.ru>
2020-08-31 17:15:27 +0400

    some changes in test logic

--- Testing/31_08_2020/testArithmetic/TestCalculator.java
+++ Testing/31_08_2020/testArithmetic/TestCalculator.java
@@ -1,24 +1,24 @@
 public class TestCalculator {
     private static void testSum() throws Exception {
-        if (Calculator.sum(-3, 3) != 0 || Calculator.sum(0, 3) != 3) {
+        if (Calculator.sum(-3, 3) != 0 && Calculator.sum(0, 3) != 3) {
             System.out.println("Method sum not working!");
         }
     }
 
     private static void testMultiply() throws Exception {
-        if(Calculator.multiply(0, 1) != 0 || Calculator.multiply(1,1) != 1) {
+        if(Calculator.multiply(0, 1) != 0 && Calculator.multiply(1,1) != 1) {
             System.out.println("Method multiply not working!");
         }
     }
 
     private static void testRemainder() throws Exception {
-        if(Calculator.remainder(5, 2) != 1 || Calculator.remainder(1,1) != 0) {
+        if(Calculator.remainder(5, 2) != 1 && Calculator.remainder(1,1) != 0) {
             System.out.println("Method remainder not working!");
         }
     }
 
     private static void testSubtract() throws Exception {
-        if(Calculator.subtract(5, 2) != 3 || Calculator.subtract(1,1) != 0) {
+        if(Calculator.subtract(5, 2) != 3 && Calculator.subtract(1,1) != 0) {
             System.out.println("Method subtract not working!");
         }
     }
@@ -30,3 +30,4 @@ public class TestCalculator {
         testSubtract();
     }
 }
+
