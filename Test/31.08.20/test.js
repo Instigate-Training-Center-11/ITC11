d902e46381066142c721af9f6d341b56ac604c47
Meliq Melqonyan <meliqmelqonyan2000@mail.ru>
2020-09-03 21:59:53 +0400

    testing

---
+++ Test/31.08.20/test.js
@@ -0,0 +1,17 @@
+describe('testing', () => {
+    before ('Before', () => {
+        cy.visit('https://google.com')
+        cy.get('input[name=q]').type('testing')
+        cy.get('[type="submit"]').contains('Google').click()
+    })
+    it('test1',() => {
+      cy.get('[data-hveid] h3').first().contains('Testing').click()
+
+      
+      
+    })
+})
+
+
+
+// [data-hveid] h3
