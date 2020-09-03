be899d0f1ebc1ae1d2e04d8bdf8e975ca1127abd
ArmenuhiKocharyan <armamik@gmail.com>
2020-09-03 21:11:40 +0400

    Added a little change

--- 03.09.2020./googleChrome/SearchTest.java
+++ 03.09.2020./googleChrome/SearchTest.java
@@ -7,15 +7,13 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.presenceOfElementLocated;
 
 public class SearchTest extends WebDriverSettings {
-
     @Test
     public void serchTest() throws InterruptedException {
         driver.get("https://www.google.com/");
         WebDriverWait wait = new WebDriverWait(driver, 1000);
 
         driver.manage().window().maximize();
-        driver.findElementByName("q").sendKeys("test");
-        driver.findElement(By.name("q")).sendKeys("testing" + Keys.ENTER);
+        driver.findElementByName("q").sendKeys("testing" + Keys.ENTER);
         WebElement firstResult = wait.until(presenceOfElementLocated(By.cssSelector("h3")));
         System.out.println((firstResult.getAttribute("textContent").toLowerCase().contains("testing".toLowerCase())));
         System.out.println(firstResult.getText());
